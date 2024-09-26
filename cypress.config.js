15e45abcfa196567de92317ddc6a77320d11d60c
DhruvSingla33 <96688509+DhruvSingla33@users.noreply.github.com>
2024-09-26 21:50:21 +0530

    updated
    updated

--- cypress.config.js
+++ cypress.config.js
@@ -16,6 +16,8 @@ module.exports = defineConfig({
     code: process.env.CODE,
     state_id:process.env.STATE_ID,
     state_password:process.env.STATE_PASSWORD,
-
+    MoHUA_id:process.env.MoHUA_ID,
+    MoHUA_password:process.env.MoHUA_PASSWORD,
+    ULB_NAME:process.env.ULB_NAME,
   },
 });
